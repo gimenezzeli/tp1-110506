b9d436db48505cd3b6a6d0e263b1de8581c57dbe
Gimenez Elizabeth <gimenezzeli@gmail.com>
2023-09-18 00:32:37 -0300

    entrega de tp1

--- README.md
+++ README.md
@@ -26,28 +26,14 @@ make valgrind-chanutron
 ##  Funcionamiento
 
 Mi programa funciona primero abriendo el archivo que recibe por parametro y despues lo lee linea por linea. Con una funcion auxiliar llamada 'cantidad_delimitadores' puedo saber cuantos delimitadores hay en esa linea. Una vez que se cuantos delimitadores tiene la linea.
-Si la linea tiene un delimitador, es decir es una linea de pokemon, intento guardarme esos datos (nombre, tipo); si el tipo que guarde en mi variable es valido lo cargo en mi pokemon_aux, que previamente habia guardado en memoria. 
+Si la linea tiene un delimitador, es decir es una linea de pokemon, intento guardarme esos datos (nombre, tipo); si el tipo que guarde en mi variable es valido lo cargo en mi pokemon_aux. 
 Si la linea tiene dos delimitadores, es decir es una linea de ataque, intento guardarme esos datos en otra variable (nombre, tipo, poder); verifico que el tipo sea el correcto y lo guardo en mi pokemon_aux.
-Una vez que ya tengo mi pokemon_aux cargado con Nombre, Tipo y tres ataques 
+Una vez que ya tengo mi pokemon_aux cargado con Nombre, Tipo y tres ataques lo guardo en mi vector dinamico info previamente asignado con un malloc y lo agrando con un realloc cuando lo cargo exitosamente. Asi con todos los pokes.
 
-
-<div align="center">
-<img width="70%" src="img/diagrama1.svg">
-</div>
-
-En el archivo `sarasa.c` la función `funcion1` utiliza `realloc` para agrandar la zona de memoria utilizada para conquistar el mundo. El resultado de `realloc` lo guardo en una variable auxiliar para no perder el puntero original en caso de error:
-
-```c
-int *vector = realloc(vector_original, (n+1)*sizeof(int));
-
-if(vector == NULL)
-    return -1;
-vector_original = vector;
-```
-
+En el archivo 'pokemon.c' la función 'pokemon_cargar_archivo' utiliza 'malloc' para agrandar la zona de memoria que va ser utilizada. Una vez verificado los datos, lo cargo a mi vector con la funcion 'cargar_poke_valido'. 
+En la funcion 'cargar_poke_valido' utilizo un realloc para agrandar mi vector de pokes.
 
 <div align="center">
-<img width="70%" src="img/diagrama2.svg">
+<img width="70%" src="img/diagrama.png">
 </div>
-
----
+--- 
--- src/pokemon.c
+++ src/pokemon.c
@@ -14,7 +14,7 @@ struct pokemon {
 	enum TIPO tipo;
 	ataque_t ataques[MAX_ATAQUE];
 	int cant_ataques;
-}; 
+};
 
 struct info_pokemon {
 	struct pokemon *pokemones;
@@ -24,11 +24,12 @@ struct info_pokemon {
 /*
  * Cuenta y devuelve la cantidad de delimitadores que tiene el string 
 */
-int cantidad_delimitadores(const char* string){
-	int contador=0;
+int cantidad_delimitadores(const char *string)
+{
+	int contador = 0;
 	size_t longitud = strlen(string);
-	for (size_t i = 0; i < longitud; i++){
-		if (string[i]==';'){
+	for (size_t i = 0; i < longitud; i++) {
+		if (string[i] == ';') {
 			(contador)++;
 		}
 	}
@@ -39,19 +40,20 @@ int cantidad_delimitadores(const char* string){
 /*
  * Parsea el tipo devolviendo el enum correspondiente
 */
-enum TIPO parsear_tipo(char tipo){
-	enum TIPO nuevo_tipo=0;
-	if (tipo=='N'){
+enum TIPO parsear_tipo(char tipo)
+{
+	enum TIPO nuevo_tipo = 0;
+	if (tipo == 'N') {
 		nuevo_tipo = NORMAL;
-	} else if (tipo=='F'){
+	} else if (tipo == 'F') {
 		nuevo_tipo = FUEGO;
-	}else if (tipo=='A'){
+	} else if (tipo == 'A') {
 		nuevo_tipo = AGUA;
-	} else if (tipo=='P'){
+	} else if (tipo == 'P') {
 		nuevo_tipo = PLANTA;
-	} else if (tipo=='E'){
+	} else if (tipo == 'E') {
 		nuevo_tipo = ELECTRICO;
-	} else if (tipo=='R'){
+	} else if (tipo == 'R') {
 		nuevo_tipo = ROCA;
 	}
 	return nuevo_tipo;
@@ -60,22 +62,26 @@ enum TIPO parsear_tipo(char tipo){
 /*
  * Valida tipo  	
 */
-bool tipo_valido(char tipo){
-	return tipo=='N' || tipo=='F' || tipo=='A' || tipo=='P' || tipo=='E' || tipo=='R';
+bool tipo_valido(char tipo)
+{
+	return tipo == 'N' || tipo == 'F' || tipo == 'A' || tipo == 'P' ||
+	       tipo == 'E' || tipo == 'R';
 }
 /**
  * Ordena alfabeticamente a informacion_pokemon_t
 **/
-informacion_pokemon_t *ordenar_alfabeticamente(informacion_pokemon_t *ip){
+informacion_pokemon_t *ordenar_alfabeticamente(informacion_pokemon_t *ip)
+{
 	pokemon_t pokemon_aux;
 	int min = 0;
 
-	for (int i = 0; i < ip->cant_pokemones; i++){
-		min=i;
-		for (int j = i+1; j < ip->cant_pokemones; j++){
-			int comparacion = strcmp(ip->pokemones[j].nombre,ip->pokemones[min].nombre);
-			if (comparacion<0){
-				min=j;
+	for (int i = 0; i < ip->cant_pokemones; i++) {
+		min = i;
+		for (int j = i + 1; j < ip->cant_pokemones; j++) {
+			int comparacion = strcmp(ip->pokemones[j].nombre,
+						 ip->pokemones[min].nombre);
+			if (comparacion < 0) {
+				min = j;
 			}
 		}
 		pokemon_aux = ip->pokemones[i];
@@ -89,13 +95,19 @@ informacion_pokemon_t *ordenar_alfabeticamente(informacion_pokemon_t *ip){
 /**
  * Carga el vector info->pokemones con los datos validos del pokemon_aux
 **/
-struct info_pokemon *cargar_poke_valido(struct pokemon *pokemon_aux, struct info_pokemon *info){
-	info->pokemones = realloc(info->pokemones, sizeof(struct pokemon)*(unsigned long)(1+info->cant_pokemones));
-	if (info->pokemones==NULL){
+struct info_pokemon *cargar_poke_valido(struct pokemon *pokemon_aux,
+					struct info_pokemon *info)
+{
+	info->pokemones =
+		realloc(info->pokemones,
+			sizeof(struct pokemon) *
+				(unsigned long)(1 + info->cant_pokemones));
+	if (info->pokemones == NULL) {
 		free(pokemon_aux);
 		free(info->pokemones);
 	}
-	strcpy(info->pokemones[info->cant_pokemones].nombre, pokemon_aux->nombre);
+	strcpy(info->pokemones[info->cant_pokemones].nombre,
+	       pokemon_aux->nombre);
 	info->pokemones[info->cant_pokemones].tipo = pokemon_aux->tipo;
 	info->pokemones[info->cant_pokemones] = *pokemon_aux;
 	info->cant_pokemones++;
@@ -103,28 +115,29 @@ struct info_pokemon *cargar_poke_valido(struct pokemon *pokemon_aux, struct info
 	return info;
 }
 
-informacion_pokemon_t *pokemon_cargar_archivo(const char *path) {
-	if (path==NULL){
+informacion_pokemon_t *pokemon_cargar_archivo(const char *path)
+{
+	if (path == NULL) {
 		return NULL;
 	}
-		
+
 	FILE *archivo = fopen(path, "r");
 
-	if (archivo==NULL){
+	if (archivo == NULL) {
 		return NULL;
 	}
-	
+
 	struct info_pokemon *info = malloc(sizeof(struct info_pokemon));
 	struct pokemon pokemon_aux;
 
-	if (info==NULL){
+	if (info == NULL) {
 		fclose(archivo);
 		return NULL;
 	}
-	
-	info->pokemones=NULL;
+
+	info->pokemones = NULL;
 	info->cant_pokemones = 0;
-	pokemon_aux.cant_ataques=0;
+	pokemon_aux.cant_ataques = 0;
 	char linea[MAX_LINEA];
 	char nombre[30];
 	char nombre_ataque[30];
@@ -133,101 +146,103 @@ informacion_pokemon_t *pokemon_cargar_archivo(const char *path) {
 	unsigned int poder;
 	bool seguir_leyendo = true;
 
-	while (fgets(linea, MAX_LINEA, archivo) != NULL && seguir_leyendo==true){
+	while (fgets(linea, MAX_LINEA, archivo) != NULL &&
+	       seguir_leyendo == true) {
+		int contador_delimitadores = cantidad_delimitadores(linea);
 
-		int contador_delimitadores=cantidad_delimitadores(linea);
-
-		if (contador_delimitadores==1){
-			if (pokemon_aux.cant_ataques!=0){
-				if (info->cant_pokemones==0){
-					seguir_leyendo=false;
-					//free(pokemon_aux);
-					free(info);
-					fclose(archivo);
-					return NULL;
-				} else{
-					seguir_leyendo=false;
-					//free(pokemon_aux);
-					fclose(archivo);
-					return info;
-				}
-				
-			}else{
-				int valores_validos = sscanf(linea, "%[^;];%c\n", nombre, &tipo);
-				if (valores_validos==2 && tipo_valido(tipo)){
-					strcpy(pokemon_aux.nombre, nombre);
-					pokemon_aux.tipo = parsear_tipo(tipo);
-				}else if (( valores_validos!=2 || !tipo_valido(tipo)) && info->cant_pokemones==0){
+		if (contador_delimitadores == 1) {
+			if (pokemon_aux.cant_ataques != 0) {
+				if (info->cant_pokemones == 0) {
 					seguir_leyendo = false;
-					//free(pokemon_aux);
-					free(info->pokemones);
 					free(info);
 					fclose(archivo);
 					return NULL;
 				} else {
 					seguir_leyendo = false;
-					//free(pokemon_aux);
-					if (info->cant_pokemones==0){
-						//free(pokemon_aux);
+					fclose(archivo);
+					return info;
+				}
+
+			} else {
+				int valores_validos = sscanf(
+					linea, "%[^;];%c\n", nombre, &tipo);
+				if (valores_validos == 2 && tipo_valido(tipo)) {
+					strcpy(pokemon_aux.nombre, nombre);
+					pokemon_aux.tipo = parsear_tipo(tipo);
+				} else if ((valores_validos != 2 ||
+					    !tipo_valido(tipo)) &&
+					   info->cant_pokemones == 0) {
+					seguir_leyendo = false;
+					free(info);
+					fclose(archivo);
+					return NULL;
+				} else {
+					seguir_leyendo = false;
+					if (info->cant_pokemones == 0) {
 						free(info);
 						fclose(archivo);
 						return NULL;
 					} else {
-						//free(pokemon_aux);
 						fclose(archivo);
 						return info;
 					}
-					
-					
 				}
 			}
-			
-		} else if (contador_delimitadores==2){
-			int ataques_valores_validos = sscanf(linea,"%[^;];%c;%u\n", nombre_ataque, &tipo_ataque, &poder);
-			if ( ataques_valores_validos == 3 && tipo_valido(tipo)){
-				strcpy(pokemon_aux.ataques[pokemon_aux.cant_ataques].nombre, nombre_ataque);
-				pokemon_aux.ataques[pokemon_aux.cant_ataques].tipo = parsear_tipo(tipo_ataque);
-				pokemon_aux.ataques[pokemon_aux.cant_ataques].poder = poder;
+
+		} else if (contador_delimitadores == 2) {
+			int ataques_valores_validos =
+				sscanf(linea, "%[^;];%c;%u\n", nombre_ataque,
+				       &tipo_ataque, &poder);
+			if (ataques_valores_validos == 3 &&
+			    tipo_valido(tipo_ataque)) {
+				strcpy(pokemon_aux
+					       .ataques[pokemon_aux.cant_ataques]
+					       .nombre,
+				       nombre_ataque);
+				pokemon_aux.ataques[pokemon_aux.cant_ataques]
+					.tipo = parsear_tipo(tipo_ataque);
+				pokemon_aux.ataques[pokemon_aux.cant_ataques]
+					.poder = poder;
 				pokemon_aux.cant_ataques++;
-				if (pokemon_aux.cant_ataques==3){
-					info = cargar_poke_valido(&pokemon_aux, info);
+				if (pokemon_aux.cant_ataques == 3) {
+					info = cargar_poke_valido(&pokemon_aux,
+								  info);
 					pokemon_aux.cant_ataques = 0;
 				}
-				
-			} else if ( ( ataques_valores_validos != 3 || !tipo_valido(tipo)) && info->cant_pokemones==0){
+
+			} else if ((ataques_valores_validos != 3 ||
+				    !tipo_valido(tipo_ataque)) &&
+				   info->cant_pokemones == 0) {
 				seguir_leyendo = false;
-				//free(pokemon_aux);
 				free(info->pokemones);
 				free(info);
 				fclose(archivo);
-				return NULL;  
-			} else {
+				return NULL;
+			} else if (!tipo_valido(tipo_ataque) &&
+				   info->cant_pokemones != 0) {
 				seguir_leyendo = false;
-				//free(pokemon_aux);
 				fclose(archivo);
 				return info;
-			} 
-
-		} else{
-			//free(pokemon_aux);
-			//free(info->pokemones);
+			}
+		} else {
 			free(info);
 			fclose(archivo);
 			return NULL;
 		}
-    }
+	}
 
-    fclose(archivo);
+	fclose(archivo);
 	return info;
 }
 
-pokemon_t *pokemon_buscar(informacion_pokemon_t *ip, const char *nombre){
-	if (ip == NULL || nombre == NULL){
+pokemon_t *pokemon_buscar(informacion_pokemon_t *ip, const char *nombre)
+{
+	if (ip == NULL || nombre == NULL) {
 		return NULL;
 	}
-	informacion_pokemon_t *ip_ordenado= ordenar_alfabeticamente(ip);
-	for (int i = 0; i < ip_ordenado->cant_pokemones; i++){
-		if (strcmp(ip_ordenado->pokemones[i].nombre, nombre)==0){
+	informacion_pokemon_t *ip_ordenado = ordenar_alfabeticamente(ip);
+	for (int i = 0; i < ip_ordenado->cant_pokemones; i++) {
+		if (strcmp(ip_ordenado->pokemones[i].nombre, nombre) == 0) {
 			return &ip_ordenado->pokemones[i];
 		}
 	}
@@ -235,78 +250,84 @@ pokemon_t *pokemon_buscar(informacion_pokemon_t *ip, const char *nombre){
 	return NULL;
 }
 
-int pokemon_cantidad(informacion_pokemon_t *ip){
-	if (ip==NULL){
+int pokemon_cantidad(informacion_pokemon_t *ip)
+{
+	if (ip == NULL) {
 		return 0;
 	}
 	return ip->cant_pokemones;
 }
 
-const char *pokemon_nombre(pokemon_t *pokemon){
-	if (pokemon==NULL){
+const char *pokemon_nombre(pokemon_t *pokemon)
+{
+	if (pokemon == NULL) {
 		return NULL;
 	}
-	
+
 	return pokemon->nombre;
 }
 
-enum TIPO pokemon_tipo(pokemon_t *pokemon){
-	if (pokemon==NULL){
+enum TIPO pokemon_tipo(pokemon_t *pokemon)
+{
+	if (pokemon == NULL) {
 		return 0;
 	}
 	return pokemon->tipo;
 }
 
 const struct ataque *pokemon_buscar_ataque(pokemon_t *pokemon,
-					   const char *nombre){
-
-	if (pokemon == NULL || nombre == NULL){
+					   const char *nombre)
+{
+	if (pokemon == NULL || nombre == NULL) {
 		return NULL;
 	}
-	
-	for (int i = 0; i < pokemon->cant_ataques; i++){
-		if (strcmp(pokemon->ataques[i].nombre, nombre)==0){
+
+	for (int i = 0; i < pokemon->cant_ataques; i++) {
+		if (strcmp(pokemon->ataques[i].nombre, nombre) == 0) {
 			return &pokemon->ataques[i];
 		}
 	}
-	
+
 	return NULL;
 }
 
 int con_cada_pokemon(informacion_pokemon_t *ip, void (*f)(pokemon_t *, void *),
-		     void *aux){
-	if (ip==NULL || f==NULL){
+		     void *aux)
+{
+	if (ip == NULL || f == NULL) {
 		return 0;
 	}
-	
+
 	int contador = 0;
-	informacion_pokemon_t *ip_ordenado= ordenar_alfabeticamente(ip);
-	
-	for (int i = 0; i < ip_ordenado->cant_pokemones; i++){
+	informacion_pokemon_t *ip_ordenado = ordenar_alfabeticamente(ip);
+
+	for (int i = 0; i < ip_ordenado->cant_pokemones; i++) {
 		f(&ip_ordenado->pokemones[i], aux);
- 		contador++;
+		contador++;
 	}
-	
+
 	return contador;
 }
 
 int con_cada_ataque(pokemon_t *pokemon,
-		    void (*f)(const struct ataque *, void *), void *aux){
-	if (pokemon==NULL || f==NULL){
+		    void (*f)(const struct ataque *, void *), void *aux)
+{
+	if (pokemon == NULL || f == NULL) {
 		return 0;
 	}
-	
+
 	int contador = 0;
-	for (int i = 0; i < pokemon->cant_ataques; i++){
+	for (int i = 0; i < pokemon->cant_ataques; i++) {
 		f(&pokemon->ataques[i], aux);
- 		contador++;
+		contador++;
 	}
-	
+
 	return contador;
 }
 
-void pokemon_destruir_todo(informacion_pokemon_t *ip){
-	if (ip!=NULL){
+void pokemon_destruir_todo(informacion_pokemon_t *ip)
+{
+	if (ip != NULL) {
 		free(ip->pokemones);
 		free(ip);
 	}
